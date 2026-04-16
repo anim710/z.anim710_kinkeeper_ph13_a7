ac968ab11f82562a3361d4564cc13f456e46365a
anim710 <anim3654@gmail.com>
2026-04-16 20:47:03 +0600

    installed react router and added the pages and navbar and footer components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router/dom'
+import { createBrowserRouter } from 'react-router';
+import RootLayout from './Layout/RootLayout';
+import Homepage from './pages/homepage/Homepage';
+import Timeline from './pages/timeline/Timeline';
+import Stats from './pages/stats/Stats';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+  },
+  {
+    path: "/homapge",
+    element: <Homepage />,
+  },
+  {
+    path: "/timeline",
+    element: <Timeline />,
+  },
+  {
+    path: "/stats",
+    element: <Stats />,
+  },
+  
+  
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-       <h2>hello, world!</h2>
+       
+        <RouterProvider router={router} />
   </StrictMode>,
 )
